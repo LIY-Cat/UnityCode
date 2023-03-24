0b5f447f0c60cc75a91da0b225c0af4b4ab8bce6
LIY-Cat <mcpe9061@naver.com>
2023-03-25 02:08:56 +0900

    CodeUp

--- 20170725_project/Assets/Script/PlayerCtrl.cs
+++ 20170725_project/Assets/Script/PlayerCtrl.cs
@@ -5,45 +5,30 @@ using UnityEngine.UI;
 
 public class PlayerCtrl : MonoBehaviour
 {
-        Rigidbody rigid;//밀도 설정 테이터타입
-        float jumpForce = 250.0f;
-        float walkForce = 5.0f;
-        GameObject scoren;
-        int score = 0;
-    // Start is called before the first frame update
-    void Start()    
+    /*
+    *간단하게 플레이어 조작하고 점수 올리 관련
+    TODO: 'this.' << delete (rigid,add_score,walkForce,jumpForce)
+    */
+        private Rigidbody rigid;//밀도
+        private float jumpForce = 250.0f;//점프 높이
+        private float walkForce = 5.0f;//한번에 이동 할 거리
+        private GameObject add_score;/*점수 올리기 */
+        private int score;//초기 점수
+
+    /*
+    * *게임 시작시 필요한 부분을 설정하는 함수
+    */
+    void Start()
     {
-        this.rigid = GetComponent<Rigidbody>();
-        this.scoren = GameObject.Find("Score");
+        Start_setUp();
+        rigid = GetComponent<Rigidbody>();
+        add_score = GameObject.Find("Score");
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        //점프
-        if(Input.GetKeyDown(KeyCode.Space) && this.rigid.velocity.y == 0.0f){
-            //트랜스폼안에 힘을위로 가하는 게 있다.
-            this.rigid.AddForce(transform.up * this.jumpForce);
-        }
-        //좌우이동
-        int key = 0;
-        //Debug.Log("Move!");//콘솔창에 띄운다. 디버그 작업
-        if(Input.GetKey(KeyCode.RightArrow)){
-            key = 1;
-            this.rigid.AddForce(transform.right * key * this.walkForce);
-
-        }else if(Input.GetKey(KeyCode.LeftArrow)){
-            key = -1;
-            this.rigid.AddForce(transform.right * key * this.walkForce);
-
-        }else if(Input.GetKey(KeyCode.UpArrow)){
-            key = 1;
-            this.rigid.AddForce(transform.forward * key * this.walkForce);
-
-        }else if(Input.GetKey(KeyCode.DownArrow)){
-            key = -1;
-            this.rigid.AddForce(transform.forward * key * this.walkForce);
-        }
+    {
+        Player_controler();
     }
 
     void OnTriggerEnter(Collider other)
@@ -51,8 +36,42 @@ public class PlayerCtrl : MonoBehaviour
         //Debug.Log("냠냠");
         score += 10;
         if(score >= 0){
-            this.scoren.GetComponent<Text>().text = "Score: " + score.ToString();
+            add_score.GetComponent<Text>().text = "Score: " + score.ToString();
         }
         Destroy(other.gameObject);
+    }
+
+    void Player_controler(){
+        //점프
+        if(Input.GetKeyDown(KeyCode.Space) && rigid.velocity.y == 0.0f){
+            //트랜스폼안에 힘을위로 가하는 게 있다.
+            rigid.AddForce(transform.up * jumpForce);
+        }
+        //좌우이동
+        int key = 0;
+        //Debug.Log("Move!");//콘솔창에 띄운다. 디버그 작업
+        if(Input.GetKey(KeyCode.RightArrow)){
+            key = 1;
+            rigid.AddForce(transform.right * key * walkForce);
+
+        }else if(Input.GetKey(KeyCode.LeftArrow)){
+            key = -1;
+            rigid.AddForce(transform.right * key * walkForce);
+
+        }else if(Input.GetKey(KeyCode.UpArrow)){
+            key = 1;
+            rigid.AddForce(transform.forward * key * walkForce);
+
+        }else if(Input.GetKey(KeyCode.DownArrow)){
+            key = -1;
+            rigid.AddForce(transform.forward * key * walkForce);
+        }
+    }
+
+    /*
+    *초기 셋팅
+    */
+    void Start_setUp(){
+        score = 0;
     }
 }
