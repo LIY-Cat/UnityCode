b7f8faf73f1883d245d1a21ae8399412fdceefb9
LIY-Cat <mcpe9061@naver.com>
2023-03-25 05:14:58 +0900

    Code Optimization

--- 20170725_project/Assets/Script/PlayerCtrl.cs
+++ 20170725_project/Assets/Script/PlayerCtrl.cs
@@ -7,71 +7,98 @@ public class PlayerCtrl : MonoBehaviour
 {
     /*
     *간단하게 플레이어 조작하고 점수 올리 관련
-    TODO: 'this.' << delete (rigid,add_score,walkForce,jumpForce)
+    *Rigidbody 밀도와 관련된 메소드
+    *GameObject 게임 오브젝트 메소드
+    *rigid: 키 입력을 받아 움직이게 한다.
+    *jumpForce: 최대 점프 높이
+    *walkForce: 한 번 움직이는 거리
+    *score_object: Score라는 게임오브젝트를 찾아서 점수를 올려준다.
+    *score: 실제 점수
+    TODO: 분석해서 코드 최적화
     */
-        private Rigidbody rigid;//밀도
-        private float jumpForce = 250.0f;//점프 높이
-        private float walkForce = 5.0f;//한번에 이동 할 거리
-        private GameObject add_score;/*점수 올리기 */
-        private int score;//초기 점수
+        private Rigidbody rigid;
+        private float jumpForce = 250.0f;
+        private float walkForce = 5.0f;
+        private GameObject scoreObject;
+        private int score = 0;
+        private Text scoreText;
 
     /*
     * *게임 시작시 필요한 부분을 설정하는 함수
     */
     void Start()
     {
-        Start_setUp();
         rigid = GetComponent<Rigidbody>();
-        add_score = GameObject.Find("Score");
+        scoreObject = GameObject.Find("Score");
+        scoreText = scoreObject.GetComponent<Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Player_controler();
     }
 
+    /*void FixedUpdate(){
+        Player_controler();
+    }*/
+    /*
+    *점수등 계산 메소드
+    TODO: 나중에 파일 나누기 할 것
+    */
     void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("냠냠");
         score += 10;
         if(score >= 0){
-            add_score.GetComponent<Text>().text = "Score: " + score.ToString();
+            scoreText.text = "Score: " + score.ToString();
         }
+        Debug.Log("이름: " + other.ToString());
         Destroy(other.gameObject);
     }
 
-    void Player_controler(){
-        //점프
-        if(Input.GetKeyDown(KeyCode.Space) && rigid.velocity.y == 0.0f){
-            //트랜스폼안에 힘을위로 가하는 게 있다.
-            rigid.AddForce(transform.up * jumpForce);
-        }
-        //좌우이동
-        int key = 0;
-        //Debug.Log("Move!");//콘솔창에 띄운다. 디버그 작업
-        if(Input.GetKey(KeyCode.RightArrow)){
-            key = 1;
-            rigid.AddForce(transform.right * key * walkForce);
-
-        }else if(Input.GetKey(KeyCode.LeftArrow)){
-            key = -1;
-            rigid.AddForce(transform.right * key * walkForce);
-
-        }else if(Input.GetKey(KeyCode.UpArrow)){
-            key = 1;
-            rigid.AddForce(transform.forward * key * walkForce);
-
-        }else if(Input.GetKey(KeyCode.DownArrow)){
-            key = -1;
-            rigid.AddForce(transform.forward * key * walkForce);
-        }
-    }
-
     /*
-    *초기 셋팅
+    *플레이어 컨트롤러 NPC등 움직임
+    TODO: 나중에 파일 나누기 할 것
     */
-    void Start_setUp(){
-        score = 0;
+    void Player_controler(){
+        /*
+        *점프
+        */
+        if(Input.GetKeyDown(KeyCode.Space) && rigid.velocity.y == 0.0f){
+            rigid.AddForce(transform.up * jumpForce * Time.deltaTime);
+        }
+        Vector3 horizontalMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);//A, D or <, >
+        Vector3 verticalMovement = new Vector3(0, 0, Input.GetAxisRaw("Vertical"));//W, S or ^, 아래 방향키
+
+        rigid.AddForce((horizontalMovement + verticalMovement).normalized * walkForce * Time.deltaTime);
+        
+        /*
+        *이동: 앞,뒤,좌,우
+        *horizontalMovement == x
+        *verticalMovement == z
+        *else if문을 사용하면은 누적이 안된다.
+        *Time.deltaTime 프레임 드랍시 밀리는 거 방지
+        *(horizontalMovement + verticalMovement).normalized: 1의 값을 반환
+        */
+        /*Vector3 horizontalMovement = new Vector3();
+        Vector3 verticalMovement = new Vector3();
+        if (Input.GetKey(KeyCode.RightArrow)){
+            horizontalMovement += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            horizontalMovement += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)){
+            verticalMovement += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            verticalMovement += new Vector3(0, 0, -1);
+        }
+        rigid.AddForce((horizontalMovement + verticalMovement).normalized * walkForce * Time.deltaTime);
+        */
+
+        
+        /* *디버그 작업
+        *Debug.Log("Move!");
+        */
     }
 }
